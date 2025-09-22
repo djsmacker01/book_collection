e90e5fcb13212702329d73820b5deb0770171106
Nurudee Adedeji <djsmacker01@gmail.com>
2025-09-22 10:58:09 +0100

    Added super admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'price', 'publication_date', 'created_at')
+    list_filter = ('genre', 'publication_date', 'created_at')
+    search_fields = ('title', 'author', 'isbn')
+    list_editable = ('price',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'author', 'isbn')
+        }),
+        ('Details', {
+            'fields': ('publication_date', 'genre', 'price', 'description')
+        }),
+        ('Cover Image', {
+            'fields': ('cover_image',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
